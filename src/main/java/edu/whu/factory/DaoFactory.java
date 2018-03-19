9a64308c288e0b1840840fd3f63187ea3ad89878
jieli213870 <jieli213870@sohu-inc.com>
2018-03-19 15:29:45 +0800

    添加dao产生

--- src/main/java/edu/whu/factory/DaoFactory.java
+++ src/main/java/edu/whu/factory/DaoFactory.java
@@ -1,8 +1,13 @@
 package edu.whu.factory;
 
 import edu.whu.config.Configuration;
+import edu.whu.constant.GlobalConstant;
+import edu.whu.model.Interface;
 import edu.whu.model.Table;
+import edu.whu.utils.StringUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -19,6 +24,33 @@ public class DaoFactory implements AbstractFactory {
 
     @Override
     public Object produce() {
+        File file = GlobalConstant.BASE_DIR_FILE;
+        String path = file.getAbsolutePath() + "/" + configuration.getDao().replaceAll("\\.", "/") + "/";
+        file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        for (int i = 0; tableList != null && i < tableList.size(); i++) {
+            createDao(path, tableList.get(i));
+        }
+
+
         return null;
+    }
+
+    private void createDao(String basePath, Table table) {
+        String name = StringUtils.firstToUpper(StringUtils.toCamelCase(table.getName())) + "Mapper";
+        File file = new File(basePath + name + ".java");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Interface inter = new Interface();
+        inter.setName(name);
+        inter.setPackageName(configuration.getDao());
+
     }
 }
---
+++ src/main/java/edu/whu/model/Interface.java
@@ -0,0 +1,67 @@
+package edu.whu.model;
+
+import java.util.List;
+
+/**
+ * @author Created By LiJie at 2018/3/19
+ */
+public class Interface {
+    private String packageName;
+    private List<String> importPackage;
+    private String name;
+    private List<String> methodList;
+
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("package " + packageName);
+        for (int i = 0; importPackage != null && i < importPackage.size(); i++) {
+            builder.append("\n" + importPackage.get(i));
+        }
+        builder.append("import org.apache.ibatis.annotations.*;\n");
+        builder.append("public interface " + name + "{\n");
+        for (int i = 0; methodList != null; i++) {
+            builder.append("\n\t" + methodList.get(i));
+        }
+        builder.append("\n}");
+
+        return builder.toString();
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public Interface setPackageName(String packageName) {
+        this.packageName = packageName;
+        return this;
+    }
+
+    public List<String> getImportPackage() {
+        return importPackage;
+    }
+
+    public Interface setImportPackage(List<String> importPackage) {
+        this.importPackage = importPackage;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Interface setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public List<String> getMethodList() {
+        return methodList;
+    }
+
+    public Interface setMethodList(List<String> methodList) {
+        this.methodList = methodList;
+        return this;
+    }
+}
