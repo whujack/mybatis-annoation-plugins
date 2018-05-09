7e9b8667857f7937ed1eb94d188f3b0144c5b732
jieli213870 <jieli213870@sohu-inc.com>
2018-05-09 14:46:30 +0800

    v1.1

--- src/main/java/com/github/whujack/config/Configuration.java
+++ src/main/java/com/github/whujack/config/Configuration.java
@@ -1,23 +1,5 @@
 package com.github.whujack.config;
 
-import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
-import com.github.whujack.utils.ReflectUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * @author Created By LiJie at 2018/03/14
  */
